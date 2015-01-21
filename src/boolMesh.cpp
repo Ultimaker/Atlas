ac57a0a6b7fae1a2395f3763512aec8bc1fa017a
Tim Kuipers <t.kuipers@ultimaker.com>
2015-01-21 12:15:01 +0100

    getFacetIntersectionlineSegment compiles!

--- src/boolMesh.cpp
+++ src/boolMesh.cpp
@@ -2,7 +2,7 @@
 
 #include <memory> // sharred_ptr
 
-#include <list> // == double-linked list, used as queue
+#include <unordered_set> //==hash_set
 
 void BooleanFVMeshOps::subtract(HE_Mesh& keep, HE_Mesh& subtracted, HE_Mesh& result)
 {
@@ -78,19 +78,77 @@ void BooleanFVMeshOps::completeFractureLine(std::shared_ptr<TriangleIntersection
 
 
 
-void BooleanFVMeshOps::getFacetIntersectionlineSegment(HE_FaceHandle& triangle1, HE_FaceHandle& triangle2, std::shared_ptr<TriangleIntersection> first, FractureLineSegment& result)
+void BooleanFVMeshOps::getFacetIntersectionlineSegment(HE_FaceHandle& triangle1, HE_FaceHandle& triangle2, std::shared_ptr<TriangleIntersection> first, FractureLinePart& result)
 {
-    typedef Graph<IntersectionPoint, TriangleIntersection>::Node Node;
-    typedef Graph<IntersectionPoint, TriangleIntersection>::Arrow Arrow;
+    typedef Graph<IntersectionPoint, IntersectionSegment>::Node Node;
+    typedef Graph<IntersectionPoint, IntersectionSegment>::Arrow Arrow;
+
+    std::unordered_map<HE_VertexHandle, Node*> vertex2node; // to know when we have closed a cycle
+    // vertices are the only places where the fracture can split
+
+    std::unordered_set<HE_FaceHandle> checked_faces;
 
     IntersectionPoint first_intersectionPoint = *first->from;
 
     Node* first_node = result.fracture.addNode(first_intersectionPoint);
 
-    Node* last_node = first_node;
+    Node* second_node = result.fracture.addNode(*first->to);
+    Arrow* first_arrow = result.fracture.connect(*first_node, *second_node, IntersectionSegment(*first, triangle1));
 
-    //std::list<Arrow> todo; // holds the prev intersectionSegment and the new intersectionPoint which should be one end of the new lineSegment
+    result.start = first_arrow;
 
+    std::list<Arrow*> todo; // holds the prev intersectionSegment and the intersectionPoint which should be the first end of the new lineSegment
+    todo.push_front(first_arrow);
+
+    while (!todo.empty())
+    {
+        Arrow* current = todo.back();
+        todo.pop_back();
+
+        Node& connectingNode = *current->to;
+        IntersectionPoint& connectingPoint = connectingNode.data;
+
+
+        switch (connectingPoint.getType())
+        {
+        case NEW: // intersection with edge
+        {
+            HE_FaceHandle newFace = connectingPoint.edge.converse().face();
+            if (checked_faces.find(newFace) == checked_faces.end())
+            {
+                checked_faces.insert(newFace);
+                std::shared_ptr<TriangleIntersection> triangleIntersection = TriangleIntersectionComputation::intersect(triangle1, newFace, connectingPoint.p());
+                addIntersectionToGraphAndTodo(connectingNode, *triangleIntersection, triangle1, newFace, vertex2node, result, todo);
+            }
+        }
+        break;
+        case VERTEX: // intersection lies exactly on vertex
+            HE_EdgeHandle first_outEdge = connectingPoint.vh.someEdge();
+            HE_FaceHandle prevFace = current->data.otherFace;
+            HE_EdgeHandle outEdge = first_outEdge;
+            do {
+                if (outEdge.face() != prevFace)
+                {
+                    HE_FaceHandle newFace = outEdge.face();
+                    if (checked_faces.find(newFace) == checked_faces.end())
+                    {
+                        checked_faces.insert(newFace);
+                        std::shared_ptr<TriangleIntersection> triangleIntersection = TriangleIntersectionComputation::intersect(triangle1, newFace, connectingPoint.p());
+                        if (triangleIntersection->intersectionType == LINE_SEGMENT)
+                        {
+                            addIntersectionToGraphAndTodo(connectingNode, *triangleIntersection, triangle1, newFace, vertex2node, result, todo);
+                        }
+                    }
+                }
+
+                outEdge = outEdge.converse().next();
+            } while ( outEdge != first_outEdge);
+        break;
+        }
+
+    }
+
+/*
     std::shared_ptr<TriangleIntersection> intersectionSegment (first);
     IntersectionPoint intersectionPoint = *first->to;
     HE_FaceHandle lastFace = triangle2;
@@ -107,7 +165,7 @@ void BooleanFVMeshOps::getFacetIntersectionlineSegment(HE_FaceHandle& triangle1,
             intersectionSegment = TriangleIntersectionComputation::intersect(triangle1, lastFace, intersectionPoint.p());
         }
         break;
-        case EXISTING: // intersection lies exactly on vertex
+        case VERTEX: // intersection lies exactly on vertex
             HE_EdgeHandle first_outEdge = intersectionPoint.vh.someEdge();
             HE_EdgeHandle outEdge = first_outEdge;
             HE_FaceHandle prevFace = lastFace;
@@ -133,11 +191,64 @@ void BooleanFVMeshOps::getFacetIntersectionlineSegment(HE_FaceHandle& triangle1,
         }
     }
 
-
+*/
 
 }
 
 
 
 
+void BooleanFVMeshOps::addIntersectionToGraphAndTodo(Node& connectingNode, TriangleIntersection& triangleIntersection, HE_FaceHandle originalFace, HE_FaceHandle newFace, std::unordered_map<HE_VertexHandle, Node*>& vertex2node, FractureLinePart& result, std::list<Arrow*> todo)
+{
+    IntersectionPoint& connectingPoint = connectingNode.data;
+
+    Node* new_node;
+
+    if      (connectingPoint.p() == triangleIntersection.to->p())  std::swap(triangleIntersection.from, triangleIntersection.to);
+    else assert (connectingPoint.p() == triangleIntersection.from->p());
+
+
+    bool new_point_is_already_done = false;
+    if      (triangleIntersection.to->getType() == VERTEX)
+    {
+        std::unordered_map<HE_VertexHandle, Node*>::const_iterator node = vertex2node.find(triangleIntersection.to->vh);
+
+        if (node == vertex2node.end()) // vertex is not yet present in graph
+        {
+            new_node = result.fracture.addNode(*triangleIntersection.to);
+            vertex2node.insert( { {triangleIntersection.to->vh, new_node} } );
+
+        } else
+        {
+            new_node = node->second;
+            new_point_is_already_done = true;
+            // dont push to todo! it is already there!
+        }
+    } else
+    {
+        new_node = result.fracture.addNode(*triangleIntersection.to);
+    }
+
+
+    Arrow* new_arrow = result.fracture.connect(connectingNode, *new_node, IntersectionSegment(triangleIntersection, newFace));
+    if (!new_point_is_already_done)
+    {
+        // check whether we reached the end of the triangle
+        switch (new_node->data.type)
+        {
+        case NEW:
+            if (new_node->data.edge.face() == originalFace)
+                result.endPoints.push_back(new_arrow);
+            else
+                todo.push_front(new_arrow);
+        break;
+        case VERTEX:
+            if (originalFace.hasVertex(new_node->data.vh))
+                result.endPoints.push_back(new_arrow);
+            else
+                todo.push_front(new_arrow);
+        break;
+        }
+    }
+}
 
--- src/boolMesh.h
+++ src/boolMesh.h
@@ -6,7 +6,10 @@
 
 #include "triangleIntersect.h"
 
-#include <unordered_map> // ==hash_map
+
+#include <list> // == double-linked list, used as queue
+
+#include <unordered_map> // == hash_map
 
 #include <string>       // std::string
 #include <sstream>      // std::stringstream,
@@ -39,13 +42,24 @@
 
 typedef FPoint3 FPoint;
 
-struct FractureLineSegment
+struct IntersectionSegment
+{
+    TriangleIntersection lineSegment; //! the intersection as computed by triangleIntersect.cpp
+    HE_FaceHandle otherFace; //! the other face than the one for which this IntersectionSegment is used
+    IntersectionSegment(TriangleIntersection lineSegment_, HE_FaceHandle otherFace_) : lineSegment(lineSegment_), otherFace(otherFace_) {};
+};
+
+//! intersection line a.k.a. fracture line is a polyline
+typedef Graph<IntersectionPoint, IntersectionSegment>::Arrow Arrow;
+typedef Graph<IntersectionPoint, IntersectionSegment>::Node Node;
+
+struct FractureLinePart
 {
     // when two triangles are coplanar, the intersection is non-linear ( => graph-like)
     // also where a vertex lies exactly on a triangle of the other mesh, the intersection can be non-linear ( => graph-like)
-    Graph<IntersectionPoint, TriangleIntersection> fracture;
-    Graph<IntersectionPoint, TriangleIntersection>::Arrow start;
-    std::vector<Graph<IntersectionPoint, TriangleIntersection>::Arrow> endPoints;
+    Graph<IntersectionPoint, IntersectionSegment> fracture;
+    Graph<IntersectionPoint, IntersectionSegment>::Arrow* start;
+    std::vector<Graph<IntersectionPoint, IntersectionSegment>::Arrow*> endPoints;
 };
 
 class BooleanFVMeshOps
@@ -53,13 +67,16 @@ class BooleanFVMeshOps
 public:
     static void subtract(HE_Mesh& keep, HE_Mesh& subtracted, HE_Mesh& result); //!< subtract one volume from another ([subtracted] from [keep])
 
-
 protected:
+
+
     HE_Mesh& keep, subtracted;
-    std::unordered_map<HE_FaceHandle, std::vector<FractureLineSegment>> face2fracturelines_mesh1;
-    std::unordered_map<HE_FaceHandle, std::vector<FractureLineSegment>> face2fracturelines_mesh2;
-    void getFacetIntersectionlineSegment(HE_FaceHandle& triangle1, HE_FaceHandle& triangle2, std::shared_ptr<TriangleIntersection> first, FractureLineSegment& result); //!< adds all intersections connected to the first which intersect with the triangle to the mapping of the triangle
+    std::unordered_map<HE_FaceHandle, std::vector<FractureLinePart>> face2fracturelines_mesh1;
+    std::unordered_map<HE_FaceHandle, std::vector<FractureLinePart>> face2fracturelines_mesh2;
+    void getFacetIntersectionlineSegment(HE_FaceHandle& triangle1, HE_FaceHandle& triangle2, std::shared_ptr<TriangleIntersection> first, FractureLinePart& result); //!< adds all intersections connected to the first which intersect with the triangle to the mapping of the triangle
     void completeFractureLine(std::shared_ptr<TriangleIntersection> first); //!< walks along (each) fracture line segment recording all fracture line segemnts in the maps, until whole fracture is explored (a fracture line can split)
+
+    void addIntersectionToGraphAndTodo(Node& connectingNode, TriangleIntersection& triangleIntersection, HE_FaceHandle originalFace, HE_FaceHandle newFace, std::unordered_map<HE_VertexHandle, Node*>& vertex2node, FractureLinePart& result, std::list<Arrow*> todo);
 };
 
 
