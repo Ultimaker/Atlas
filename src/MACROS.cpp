f59ffd08f5bda43430d87c1d0f78608a9cebc595
Tim Kuipers <t.kuipers@ultimaker.com>
2015-01-28 15:40:08 +0100

    MACROS in separate file

---
+++ src/MACROS.cpp
@@ -0,0 +1,26 @@
+#include "MACROS.h"
+
+
+#define DEBUG_HERE std::cerr << __FILE__ << " : " << __LINE__ << std::endl
+
+
+
+
+#define ENUM(name, ...) enum class name { __VA_ARGS__, __COUNT}; \
+inline std::ostream& operator<<(std::ostream& os, name value) { \
+std::string enumName = #name; \
+std::string str = #__VA_ARGS__; \
+int len = str.length(); \
+std::vector<std::string> strings; \
+std::ostringstream temp; \
+for(int i = 0; i < len; i ++) { \
+if(isspace(str[i])) continue; \
+        else if(str[i] == ',') { \
+        strings.push_back(temp.str()); \
+        temp.str(std::string());\
+        } \
+        else temp<< str[i]; \
+} \
+strings.push_back(temp.str()); \
+os << enumName << "::" << strings[static_cast<int>(value)]; \
+return os;}
